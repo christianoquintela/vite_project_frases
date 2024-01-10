32eaa8bd81f7ba14452ae0343a939b67a15c5d59
Christiano Quintela <christianoquintela@gmail.com>
2024-01-10 19:22:28 -0300

    Post e Get funcionando corretamente.

--- src/pages/cadastro/cadastro.tsx
+++ src/pages/cadastro/cadastro.tsx
@@ -1,18 +1,55 @@
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
 import './cadastro.css';
+import axios from 'axios';
+import { url } from '../../services/index';
+import { useState } from 'react';
 
 export default function Cadastro() {
-  return (
-    <>
-      <Header />
-      <section>
-        <input type='text' />
-        <input type='text' />
-        {/* <textarea name='frase' id='' cols='30' rows='10'></textarea> */}
-      </section>
+    const [titulo, setTitulo] = useState('');
+    const [frase, setFrase] = useState('');
+    const [autor, setAutor] = useState('');
 
-      <Footer />
-    </>
-  );
+    const createFrase = () => {
+        axios.post(` ${url}/cadastro`, { titulo, frase, autor }).then((res) => {
+            console.log(res.data);
+        });
+        setTitulo('');
+        setFrase('');
+        setAutor('');
+    };
+
+    // if (!frase) return 'Sem frase!';
+    return (
+        <div className='container'>
+            <Header />
+            <section className="container_cad">
+              <label>Titulo:</label>
+                <input
+                value={titulo}
+                    type="text"
+                    placeholder="titulo"
+                    onChange={(e) => setTitulo(e.target.value)}
+                />
+                <label>Frase:</label>
+                <input
+                value={frase}
+                    type="text"
+                    placeholder="frase"
+                    onChange={(e) => setFrase(e.target.value)}
+                />
+                <label>Autor da Frase:</label>
+                <input
+                value={autor}
+                    type="text"
+                    placeholder="autor"
+                    onChange={(e) => setAutor(e.target.value)}
+                />
+                {/* <textarea name='frase' id='' cols='30' rows='10'></textarea> */}
+            </section>
+            <button onClick={createFrase} className='button'>Criando a frase!</button>
+
+            <Footer />
+        </div>
+    );
 }
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,32 +1,60 @@
+//Components
 import AreaTexto from '../../components/areaTexto/areaTexto';
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
+//Css
 import './home.css';
+//Libs
 
 import axios from 'axios';
+import { url } from '../../services/index';
+import { useState } from 'react';
 
-const url = 'http://localhost:8080';
+interface PropsInfos {
+    id_frase?: string;
+    titulo_frase?: string;
+    frase: string;
+    autor?: string;
+}
 
 const Home = () => {
+    const [infos, setInfos] = useState<PropsInfos | null>();
 
-  axios.post(url,{})//Continuar amanhar a partir daqui!
-  return (
-    <div className='container_home'>
-      <Header />
-<main className='container_main'>
-  <AreaTexto id={`${}`} titulo={`${}`} frase={`${}`} autor={`${}`} />
-  <button>Frase do dia</button>
-  <button>Frase do dia StarWars</button>
-  <button>Dica de programação</button>
-  <button>Emoji do dia</button>
-  <button>Comentários:</button>
+    // useEffect(() => {
+    //     axios.get(url).then((res) => {
+    //         console.log(res.data);
+    //         console.log(res.data.frase);
+    //         setInfos(res.data);
+    //     });
+    //   }, []);
 
-      
-</main>
-      
-      <Footer />
-    </div>
-  );
+    const fraseDia = () => {
+        axios.get(`${url}/1`).then((res) => {
+            // console.log(res.data);
+            // console.log(res.data.frase);
+            setInfos(res.data);
+        });
+    };
+    return (
+        <div className="container_home">
+            <Header />
+            <main className="container_main">
+                <AreaTexto
+                    id={`${infos?.id_frase}`}
+                    titulo={`${infos?.titulo_frase}`}
+                    frase={`${infos?.frase}`}
+                    autor={`${infos?.autor}`}
+                />
+                <button onClick={fraseDia}>Frase do dia</button>
+                <button>Frase do dia StarWars</button>
+                <button>Dica de programação</button>
+                <button>Emoji do dia</button>
+                <button>Comentários:</button>
+            </main>
+
+            <Footer />
+        </div>
+    );
 };
 
 export default Home;
